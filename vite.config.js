05318db3f702b2361d690a075074ef8975a9d9e1
Juliana Argaez <argaez8533025@gmail.com>
2024-04-13 19:14:07 -0500

    se corrije el main.jsx y la configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Añade un alias para que puedas importar archivos Sass sin especificar la extensión.
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        // Habilita el soporte de sintaxis SCSS
-        // (Si estás usando el sintaxis de Sass en lugar de SCSS, cambia 'scss' a 'sass')
-        prependData: `@use '@/styles/variables' as *;`, // puedes agregar variables comunes aquí
-      },
-    },
-  },
 })
