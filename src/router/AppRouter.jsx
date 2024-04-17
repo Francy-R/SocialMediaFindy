310069f72e704bca3a51cce640d9a0c88b5a2acc
Judith Vanessa Mallama Pascumal <judith.mallama@udea.edu.co>
2024-04-17 17:11:55 -0500

    Logica del login terminada sin estilo

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
@@ -7,11 +7,12 @@ import Register from "../pages/login/Register";
 export const AppContext = createContext(null);
 
 const AppRouter = () => {
+    const [user, setUser] = useState({})
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element ={<Home />} />
-                <Route path="login" element={<Login setUser={setUser} />} />
+                <Route path="login" element={<Login setUser={setUser}/>} />
                 <Route path="register" element = {<Register />} />
             </Routes>
 
